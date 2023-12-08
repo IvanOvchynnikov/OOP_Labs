213d6cae7b935b0e85d9d09997b9383d84688892
IvanOvchynnikov <ovchinic76@gmail.com>
2023-12-08 16:51:53 +0100

    Lab_10

--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,74 @@
-import javax.swing.text.Style;
-import java.lang.Math;
-import java.sql.Array;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * The Main class reads input values from a file, performs calculations,
+ * and writes the result to an output file.
+ */
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter the length of array: ");
-        int length=scanner.nextInt();
-        OvchiList current=new OvchiList(length);
-        System.out.print("Please enter the elements of array: ");
-        for(int i=0; i<length; i++){
-            current.arr[i]=scanner.nextInt();
+    /**
+     * Reads a double value from the given Scanner.
+     *
+     * @param scanner The Scanner to read from.
+     * @return The double value read.
+     */
+    public static double scanDouble(Scanner scanner) {
+        if (scanner.hasNextDouble()) {
+            return scanner.nextDouble();
+        } else {
+            System.out.println("The file has incorrect datatype of values");
+            System.exit(0);
         }
-        int type=0;
-        while(type!=-1){
-            System.out.println("Choose the type of the operation you want to do:");
-            System.out.println("1: Print the minimal value of array");
-            System.out.println("2: Print the maximal value of array");
-            System.out.println("3: Print the sum of array");
-            System.out.println("4: Print the average value of array");
-            System.out.println("5: Print odd elements of array");
-            System.out.println("6: Print even elements of array");
-            System.out.println("-1: Stop the program");
-            type=scanner.nextInt();
-            switch (type){
-                case 1:
-                    current.getMin();
-                    break;
-                case 2:
-                    current.getMax();
-                    break;
-                case 3:
-                    current.getSum();
-                    break;
-                case 4:
-                    current.getAverage();
-                    break;
-                case 5:
-                    current.getOdd();
-                    break;
-                case 6:
-                    current.getEven();
-                    break;
-            }
-        }
+        return 0;
     }
-}
 
-class OvchiList{
-    int[] arr;
-    OvchiList(int length){
-        arr=new int[length];
-    }
-    public void getMin(){
-        int min=arr[0];
-        for(int i=1; i<arr.length; i++){
-            min=Math.min(min,arr[i]);
+    /**
+     * Reads an integer value from the given Scanner.
+     *
+     * @param scanner The Scanner to read from.
+     * @return The integer value read.
+     */
+    public static int scanInt(Scanner scanner) {
+        if (scanner.hasNextInt()) {
+            return scanner.nextInt();
+        } else {
+            System.out.println("The file has incorrect datatype of values");
+            System.exit(0);
         }
-        System.out.println("The minimum of array is: "+min);
+        return 0;
     }
-    public void getMax(){
-        int max=arr[0];
-        for(int i=1; i<arr.length; i++){
-            max=Math.max(max,arr[i]);
+
+    /**
+     *
+     * @param args Command-line arguments (not used in this program).
+     */
+    public static void main(String[] args) {
+        File file = new File("input.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            double x = scanDouble(scanner);
+            double y = scanDouble(scanner);
+            int z = scanInt(scanner);
+            y /= 100;
+            z /= 12;
+            double ans = x * Math.pow((1 + y), z);
+
+            try {
+                FileWriter answer = new FileWriter("answer.txt", true);
+                answer.write("Your profit is: " + (ans - x));
+                answer.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Input mismatch. Check data types in the file.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println("The maximum of array is: "+max);
-    }
-    public void getSum(){
-        int sum=0;
-        for(int i=0; i<arr.length; i++){
-           sum+=arr[i];
-        }
-        System.out.println("The sum of array is: "+sum);
-    }
-    public void getAverage(){
-        int sum=0;
-        for(int i=0; i<arr.length; i++){
-            sum+=arr[i];
-        }
-        System.out.println("The sum of array is: "+sum/arr.length);
-    }
-    public void getOdd(){
-        System.out.println("The odd elements of array:");
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]%2!=0) System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-    }
-    public void getEven(){
-        System.out.println("The even elements of array:");
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]%2==0) System.out.print(arr[i]+" ");
-        }
-        System.out.println();
     }
 }
